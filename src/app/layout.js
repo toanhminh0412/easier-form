218b8ffb67cffce5b04f2a13bb4771998a0dcbd3
Archie To <toanhminh0412@gmail.com>
2024-05-04 16:25:04 -0700

    Move the script tag to the bottom of the body tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,11 @@ export default function RootLayout({ children }) {
             <head>
                 {/* Font Awesome icons */}
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossOrigin="anonymous" referrerPolicy="no-referrer"></script>
             </head>
-            <body className={`${inter.className} h-full`}>{children}</body>
+            <body className={`${inter.className} h-full`}>
+                {children}
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js" integrity="sha512-u3fPA7V8qQmhBPNT5quvaXVa1mnnLSXUep5PS1qo5NRzHwG19aHmNJnj1Q8hpA/nBWZtZD4r4AX6YOt5ynLN2g==" crossOrigin="anonymous" referrerPolicy="no-referrer"></script>
+            </body>
         </html>
     );
 }
