49f6cff6c125994ba5a7af38fb0697e7765eeff6
Archie To <toanhminh0412@gmail.com>
2024-06-22 16:48:46 -0700

    Fix 'Server Functions cannot be called during initial render' error

--- src/app/signin/layout.js
+++ src/app/signin/layout.js
@@ -1,6 +1,15 @@
+import { cookies } from "next/headers"
+import { redirect } from "next/navigation"
+
 import Navbar from "@/components/navbars/Navbar"
 
 export default function FormLayout({ children }) {
+    const signedInCookie = cookies().get("signedIn");
+    const signedIn = signedInCookie && signedInCookie.value === "true" ? true : false;
+    if (signedIn) {
+        redirect("/");
+    }
+
     return (
         <div>
             <Navbar signedIn={false}/>
