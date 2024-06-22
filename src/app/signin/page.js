136fb5e0150c9fbf33e208529cb77ce155bd5014
Archie To <toanhminh0412@gmail.com>
2024-06-22 16:34:01 -0700

    Render a loading state on signin page

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -113,8 +113,9 @@ export default function Page() {
                                 <button
                                     type="submit"
                                     className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    disabled={loading}
                                 >
-                                    Sign in
+                                    {loading ? "Signing in..." : "Sign in"}
                                 </button>
                                 </div>
                             </form>
