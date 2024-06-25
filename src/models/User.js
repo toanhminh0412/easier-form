02d7cf4044010702f578fa34e830461406709823
Archie To <toanhminh0412@gmail.com>
2024-06-24 22:17:00 -0700

    Make password a non-required field

--- src/models/User.js
+++ src/models/User.js
@@ -16,7 +16,7 @@ const userSchema = new Schema({
     },
     password: {
         type: String,
-        required: true
+        required: false
     },
     isEmailVerified: {
         type: Boolean,
