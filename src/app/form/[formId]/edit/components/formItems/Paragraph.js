6c54f7cb1f53ec89e7a32c94f740514ab637b11d
Archie To <toanhminh0412@gmail.com>
2024-07-16 19:53:36 -0700

    Make paragraph text small

--- src/app/form/[formId]/edit/components/formItems/Paragraph.js
+++ src/app/form/[formId]/edit/components/formItems/Paragraph.js
@@ -3,7 +3,7 @@ import SafeHtml from "@/components/SafeHtml"
 export default function Paragraph({ item }) {
     return (
         <div>
-            <SafeHtml className="text-gray-900" htmlContent={item.text}></SafeHtml>
+            <SafeHtml className="text-gray-900 text-sm" htmlContent={item.text}></SafeHtml>
         </div>
     )
 }
