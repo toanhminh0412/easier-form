6f136657bacaae154937d38dc76759f0e88a26d7
Archie To <toanhminh0412@gmail.com>
2024-07-23 08:27:57 -0700

    Update form showcase

--- src/components/landing/Showcase.js
+++ src/components/landing/Showcase.js
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 /*
   This example requires some changes to your config:
   
@@ -15,30 +17,27 @@
 const products = [
     {
         id: 1,
-        name: 'Focus Paper Refill',
-        href: '#',
-        price: '$13',
-        description: '3 sizes available',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-01.jpg',
-        imageAlt: 'Person using a pen to cross a task off a productivity paper card.',
+        name: 'Get a quote',
+        href: 'https://easierform.com/viewform/getaquotedemo',
+        description: 'For lawn mowing, window cleaning, hauling and moving services',
+        imageSrc: 'https://firebasestorage.googleapis.com/v0/b/easier-form.appspot.com/o/public%2FGet%20a%20quote.png?alt=media&token=82cbeea5-9809-4bea-949e-53ebef4becf7',
+        imageAlt: 'Get a quote form demo',
     },
     {
         id: 2,
-        name: 'Focus Card Holder',
-        href: '#',
-        price: '$64',
-        description: 'Walnut',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-02.jpg',
-        imageAlt: 'Paper card sitting upright in walnut card holder on desk.',
+        name: 'General Admission Registration',
+        href: 'https://www.easierform.com/viewform/admissionregistrationdemo',
+        description: 'Admission registration form for an event that includes food and multiple activities',
+        imageSrc: 'https://firebasestorage.googleapis.com/v0/b/easier-form.appspot.com/o/public%2Fgeneral_admission_registration.png?alt=media&token=d4c90d70-3c9d-4f83-9864-6f9e853008e4',
+        imageAlt: 'Demo form for general admission registration',
     },
     {
         id: 3,
-        name: 'Focus Carry Case',
-        href: '#',
-        price: '$32',
-        description: 'Heather Gray',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/category-page-01-image-card-03.jpg',
-        imageAlt: 'Textured gray felt pouch for paper cards with snap button flap and elastic pen holder loop.',
+        name: 'Hotel booking',
+        href: 'https://www.easierform.com/viewform/hotelbookingdemo',
+        description: 'Room booking form for a hotel with multiple room types and add-ons',
+        imageSrc: 'https://firebasestorage.googleapis.com/v0/b/easier-form.appspot.com/o/public%2Fhotel_booking.png?alt=media&token=bf0770a4-e4ef-4382-ae58-450554c6adbe',
+        imageAlt: 'Demo form for hotel booking',
     },
 ]
   
@@ -49,24 +48,29 @@ export default function Showcase() {
                 <h2 id="products-heading" className="sr-only">
                 Products
                 </h2>
-                <h2 className="text-xl font-bold text-gray-900 mb-4">Check out forms that are built by <span className="text-primary">EasierForm</span></h2>
+                <h2 className="text-xl font-bold text-gray-900 mb-4">Check out forms that were built by <span className="text-primary">EasierForm</span></h2>
 
                 <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                     {products.map((product) => (
-                        <a key={product.id} href={product.href} className="group">
-                        <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg sm:aspect-h-3 sm:aspect-w-2">
-                            <img
-                            src={product.imageSrc}
-                            alt={product.imageAlt}
-                            className="h-full w-full object-cover object-center group-hover:opacity-75"
-                            />
-                        </div>
-                        <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
-                            <h3>{product.name}</h3>
-                            <p>{product.price}</p>
-                        </div>
-                        <p className="mt-1 text-sm italic text-gray-500">{product.description}</p>
-                        </a>
+                        <Link key={product.id} href={product.href} target="_blank" className="group">
+                            <div className="aspect-h-5 aspect-w-6 w-full rounded-lg border border-gray-200 shadow-md overflow-hidden">
+                                <img
+                                    src={product.imageSrc}
+                                    alt={product.imageAlt}
+                                    className="h-full w-full object-contain object-center group-hover:opacity-75"
+                                />
+                            </div>
+                            <div className="mt-4 flex items-center justify-between text-base font-medium text-gray-900">
+                                <h3>{product.name}</h3>
+                                {/* <Link 
+                                    href={product.href}
+                                    target="_blank"
+                                    className="rounded-md bg-indigo-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                        View form
+                                </Link> */}
+                            </div>
+                            <p className="mt-1 text-sm italic text-gray-500">{product.description}</p>
+                        </Link>
                     ))}
                 </div>
             </div>
