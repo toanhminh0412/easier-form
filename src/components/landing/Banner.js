05884ccdd3c4b273bc997c4316e39173d289321c
Archie To <toanhminh0412@gmail.com>
2024-08-06 19:29:50 -0700

    Update frontpage

--- src/components/landing/Banner.js
+++ src/components/landing/Banner.js
@@ -15,7 +15,7 @@ export default function Banner() {
     return (
         <div className="flex items-center gap-x-6 bg-indigo-600 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
             <p className="text-sm leading-6 text-white">
-                <Link href="https://easierform.com/viewform/easierform" target="_blank">
+                <Link href="https://easierform.com/viewform/easierform">
                     <strong className="font-semibold">Need us to build a form just for you?</strong>
                     <svg viewBox="0 0 2 2" aria-hidden="true" className="mx-2 inline h-0.5 w-0.5 fill-current">
                         <circle r={1} cx={1} cy={1} />
